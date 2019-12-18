0da0fa72b471a844b2011416288d205b65c45495
Diana Badas <diana.badas@gmail.com>
2019-12-18 16:28:15 +0000

    testing

--- testing.py
+++ testing.py
@@ -1,1 +1,17 @@
+linkedlist = LinkedList();
 
+linkedlist.insertStart(12)
+linkedlist.insertStart(122)
+linkedlist.insertStart(8)
+linkedlist.insertEnd(31)
+
+linkedlist.traverseList();
+ => 8,122,12,31
+
+linkedlist.size1();
+  => 4
+
+linkedlist.remove(31);
+
+linkedlist.size1();
+  => 3
